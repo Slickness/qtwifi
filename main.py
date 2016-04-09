33ccd10a57b0bdb31631495488fcd01596a91d2e
slickness <randyh1981@gmail.com>
2016-04-09 16:10:45 -0400

    getting GUI set

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print ("hello world")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class Main(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300,300,300,220)
+        self.setWindowTitle('Title')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    main = Main()
+
+    sys.exit(app.exec_())
